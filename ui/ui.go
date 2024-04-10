73bb5ef956fbaa071d06d4b5d362c4a9a4e2f91b
Dr-Sirius <mcnew1721@gmail.com>
2024-04-09 23:12:44 -0400

    adjusted layout

--- ui/ui.go
+++ ui/ui.go
@@ -3,11 +3,14 @@ package ui
 import (
 	"can-i-go-yet/src"
 	"can-i-go-yet/src/scheduler"
+	"image/color"
+
 	"time"
 
 	"fyne.io/fyne/v2"
 	_ "fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/layout"
 	_ "fyne.io/fyne/v2/theme"
@@ -18,13 +21,22 @@ func Run() {
 	app := app.New()
 	//app.Settings().SetTheme(&defaultTheme{})
 	myWindow := app.NewWindow("Can I Go Yet?")
-	
+	todayLBL := canvas.NewText("Today's Schedule", color.White)
+	todayLBL.TextSize = 35
+
 	content := container.New(
-		layout.NewAdaptiveGridLayout(3),
-		widget.NewSeparator(),
-		widget.NewLabel("Today's Schedules"),
-		DailyList(),
+		layout.NewAdaptiveGridLayout(4),
+		layout.NewSpacer(),
+		layout.NewSpacer(),
+		container.NewGridWithRows(
+			5,
+			layout.NewSpacer(),
+			layout.NewSpacer(),
+			todayLBL,
+			DailyList(),
+		),
 	)
+	
 	myWindow.SetContent(content)
 	myWindow.Resize(fyne.NewSize(800, 600))
 	myWindow.ShowAndRun()
@@ -37,10 +49,12 @@ func DailyList() *widget.List {
 			return len(data.Schedules)
 		},
 		func() fyne.CanvasObject {
-			return widget.NewLabel("template")
+			lbl := canvas.NewText("template",color.White)
+			lbl.TextSize = 15
+			return lbl
 		},
 		func(i widget.ListItemID, o fyne.CanvasObject) {
-			o.(*widget.Label).SetText(data.Schedules[i].String())
+			o.(*canvas.Text).Text = data.Schedules[i].String()
 		},
 	)
 }
